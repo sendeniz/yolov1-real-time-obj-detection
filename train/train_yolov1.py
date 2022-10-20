2c64ce4d799c4ccefe582cce9f05ad2539a4cdcf
Deniz Sen <denizsen@protonmail.com>
2022-10-20 21:07:54 +0200

    adjusted train

--- train/train_yolov1.py
+++ train/train_yolov1.py
@@ -19,12 +19,14 @@ from models.yolov1net_darknet import YoloV1Net
 from models.yolov1net_vgg19bn import YoloV1_Vgg19bn
 from models.yolov1net_resnet18 import YoloV1_Resnet18
 from models.yolov1net_resnet50 import YoloV1_Resnet50
-from models.tiny_yolov1net_resnet18 import Tiny_YoloV1_Resnet18
+from models.tiny_yolov1net import Tiny_YoloV1
+from models.tiny_yolov1net_mobilenetv3_large import Tiny_YoloV1_MobileNetV3_Large
+from models.tiny_yolov1net_mobilenetv3_small import Tiny_YoloV1_MobileNetV3_Small
+from models.tiny_yolov1net_squeezenet import Tiny_YoloV1_SqueezeNet
 
 import cv2 as cv
 import numpy as np 
 from numpy import genfromtxt
-
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 batch_size = 4
@@ -33,7 +35,8 @@ epochs = 140
 nworkers = 2
 pin_memory = True
 
-continue_training = True
+# to resume training from a previous checkpoint set to True
+continue_training = False
 
 img_dir = 'data/images/'
 label_dir = 'data/labels/'
@@ -43,8 +46,10 @@ use_vgg19bn_backbone = False
 use_original_darknet_backbone = False
 use_resnet18_backbone = False
 use_resnet50_backbone = False
-use_tiny_resnet18_backbone = True
-
+use_tiny_backbone = False
+use_mobilenetv3_large_backbone = False
+use_mobilenetv3_small_backbone = True
+use_squeezenet_backbone = False
 check_image_transform = False
 save_model = True
 
@@ -52,7 +57,10 @@ model_names = ['vgg19bn_orig_lr_',
                 'vgg19bn_adj_lr_',
                 'resnet18_adj_lr_',
                 'resnet50_adj_lr_',
-                'tiny_resnet18_adj_lr_']
+                'tiny_adj_lr_',
+                'mobilenetv3_large_tiny_adj_lr_',
+                'mobilenetv3_small_tiny_adj_lr_',
+                'squeezenet_tiny_adj_lr_']
 
 if lr_sched_original == True and use_vgg19bn_backbone == True:
     lr = 0.001
@@ -70,9 +78,21 @@ elif lr_sched_adjusted == True and use_resnet50_backbone == True:
     lr =  0.00001
     current_model = model_names[3]
     path_cpt_file = f'cpts/{current_model}yolov1.cpt'
-elif lr_sched_adjusted == True and use_tiny_resnet18_backbone == True:
+elif lr_sched_adjusted == True and use_tiny_backbone == True:
     lr =  0.00001
     current_model = model_names[4]
+    path_cpt_file = f'cpts/{current_model}yolov1.cpt'
+elif lr_sched_adjusted == True and use_mobilenetv3_large_backbone == True:
+    lr =  0.00001
+    current_model = model_names[5]
+    path_cpt_file = f'cpts/{current_model}yolov1.cpt'
+elif lr_sched_adjusted == True and use_mobilenetv3_small_backbone == True:
+    lr =  0.00001
+    current_model = model_names[6]
+    path_cpt_file = f'cpts/{current_model}yolov1.cpt'
+elif lr_sched_adjusted == True and use_squeezenet_backbone == True:
+    lr =  0.00001
+    current_model = model_names[7]
     path_cpt_file = f'cpts/{current_model}yolov1.cpt'
 
 
@@ -164,10 +184,22 @@ def main():
         model = YoloV1_Resnet50(S = 7, B = 2, C = 20).to(device)
         print("Petrained resnet 50 network initalized as a backbone.")
     
-    elif use_tiny_resnet18_backbone == True:
-        model = Tiny_YoloV1_Resnet18(S = 7, B = 2, C = 20).to(device)
-        print("Petrained tiny resnet 18 yolo network initalized as a backbone.")
+    elif use_tiny_backbone == True:
+        model = Tiny_YoloV1(S = 7, B = 2, C = 20).to(device)
+        print("Untrained tiny yolov1 network initalized as a backbone.")
 
+    elif use_mobilenetv3_large_backbone == True:
+        model = Tiny_YoloV1_MobileNetV3_Large(S = 7, B = 2, C = 20).to(device)
+        print("Pretrained mobilenet v3 large network initalized as a backbone.")
+    
+    elif use_mobilenetv3_small_backbone == True:
+        model = Tiny_YoloV1_MobileNetV3_Small(S = 7, B = 2, C = 20).to(device)
+        print("Pretrained mobilenet v3 small network initalized as a backbone.")
+    
+    elif use_squeezenet_backbone == True:
+        model = Tiny_YoloV1_SqueezeNet(S = 7, B = 2, C = 20).to(device)
+        print("Pretrained squeezenet network initalized as a backbone.")
+    
     else:
         print("No backbone was specified. Please check the boolean flags in train_yolov1.py and set the flag for supported backbones to True.")
     
@@ -190,7 +222,6 @@ def main():
         train_mAP_lst = list(genfromtxt(f"results/{current_model}train_mAP.txt", delimiter=','))
         test_loss_lst = list(genfromtxt(f"results/{current_model}test_loss.txt", delimiter=','))
         test_mAP_lst = list(genfromtxt(f"results/{current_model}test_mAP.txt", delimiter=','))
-
 
     train_dataset = VOCData(csv_file = 'data/train.csv',
                             img_dir = img_dir, label_dir = label_dir, 
@@ -241,7 +272,6 @@ def main():
                 if epoch + last_epoch > 110:
                     g['lr'] = 0.000001
 
-
         # for training data
         pred_bbox, target_bbox = get_bboxes(train_loader, model, iou_threshold = 0.5, 
                                           threshold = 0.4)
@@ -261,7 +291,7 @@ def main():
         print(f"Learning Rate:", optimizer.param_groups[0]["lr"])
         print(f"Epoch:{epoch + last_epoch + 1 }  Train[Loss:{train_loss_value} mAP:{train_mAP_val}]  Test[Loss:{test_loss_value} mAP:{test_mAP_val}]")
   
-        if save_model == True and ( (epoch + last_epoch + 1 ) % 5) == 0 or epoch + last_epoch == epochs - 1 :
+        if save_model == True and ( (epoch + last_epoch + 1 ) % 2) == 0 or epoch + last_epoch == epochs - 1 :
             torch.save({
                 'epoch': epoch + last_epoch,
                 'model_state_dict': model.state_dict(),
