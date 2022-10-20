bb7deeae5d37075eee415ca38a583fe90c94bc86
Deniz Sen <denizsen@protonmail.com>
2022-10-20 15:22:50 +0200

    added second data download mirror

--- utils/get_data_macos_pjreddie.sh
+++ utils/get_data_macos_pjreddie.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+wget http://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar 
 tar xf VOCtrainval_11-May-2012.tar
 rm VOCtrainval_11-May-2012.tar
 
-wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+wget http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar 
 tar xf VOCtrainval_06-Nov-2007.tar
 rm VOCtrainval_06-Nov-2007.tar
 
-wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
+wget http://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
 tar xf VOCtest_06-Nov-2007.tar
 rm VOCtest_06-Nov-2007.tar
 
 #wget https://pjreddie.com/media/files/voc_label.py
-#python3 utils/voc_label.py
+#mv voc_label.py utils/ 
+python3 utils/voc_label.py
 
 cat 2007_train.txt 2007_val.txt 2012_*.txt > train.txt
 cp 2007_test.txt test.txt
@@ -23,10 +24,12 @@ mkdir data
 mkdir data/images
 mkdir data/labels
 
-mv VOCdevkit/VOC2007/JPEGImages/*.jpg data/images/                                      
-mv VOCdevkit/VOC2012/JPEGImages/*.jpg data/images/                                      
-mv VOCdevkit/VOC2007/labels/*.txt data/labels/                                          
-mv VOCdevkit/VOC2012/labels/*.txt data/labels/
+for f in VOCdevkit/VOC2007/JPEGImages/*.jpg; do mv "$f" data/images/; done
+for f in VOCdevkit/VOC2012/JPEGImages/*.jpg; do mv "$f" data/images/; done
+
+for f in VOCdevkit/VOC2007/labels/*.txt; do mv "$f" data/labels/; done
+for f in  VOCdevkit/VOC2012/labels/*.txt; do mv "$f" data/labels/; done
+
 
 rm -rf VOCdevkit/
 python3 utils/generate_csv.py
--- utils/get_data_pjreddie.sh
+++ utils/get_data_pjreddie.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+wget http://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar 
 tar xf VOCtrainval_11-May-2012.tar
 rm VOCtrainval_11-May-2012.tar
 
-wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+wget http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar 
 tar xf VOCtrainval_06-Nov-2007.tar
 rm VOCtrainval_06-Nov-2007.tar
 
-wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
+wget http://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
 tar xf VOCtest_06-Nov-2007.tar
 rm VOCtest_06-Nov-2007.tar
 
